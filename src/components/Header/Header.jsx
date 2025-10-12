9c444694be4ce979a2942f75dc5dfcef562cb1e0
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2025-10-11 21:01:03 -0600

    Reloaded Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import './Header.css';
+import logo from '../../assets/film-icon.svg'
+import Busqueda from '../Busqueda/Busqueda';
+import { Link } from 'react-router-dom';
+
+export default function Header(){
+    return(
+        <header>
+            <img src={logo} alt="Logo" />
+            <Busqueda/>
+            <nav>
+                <ul>
+                    <li><Link to="/peliculas">Películas</Link></li>
+                    <li><Link to="/series">Series</Link></li>
+                    <li><Link to="/personas">Personas</Link></li>
+                </ul>
+            </nav>
+            
+
+        </header>
+        
+    );
+}
