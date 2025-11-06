caa3409c79cd5188ec6ad95adaf9835c5af24774
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2025-11-05 19:49:40 -0600

    Fixed release year

--- src/components/ResultadoBusqueda/PeliculaEncontrada/PeliculaEncontrada.jsx
+++ src/components/ResultadoBusqueda/PeliculaEncontrada/PeliculaEncontrada.jsx
@@ -12,7 +12,7 @@ const PeliculaEncontrada = ({id, mediaType, posterPath, titulo, release_date, re
                 <img src={posterPath ? `https://image.tmdb.org/t/p/w500${posterPath}` : ImageNotFound} alt={titulo} onClick={()=>navigate(`/${mediaType}/${id}`)}/>
             </div>
             <div className="contenedorInformacion">
-                <h3 className="titulo" onClick={()=>navigate(`/${mediaType}/${id}`)}> {titulo ? titulo : ''} <span>({sliceYear(release_date)})</span></h3>
+                <h3 className="titulo" onClick={()=>navigate(`/${mediaType}/${id}`)}> {titulo ? titulo : ''} <span>{release_date ? `(${sliceYear(release_date)})` : ''}()</span></h3>
                 <div className="resumen">{resumen ? resumen : ''}</div>
             </div>
         </div>
