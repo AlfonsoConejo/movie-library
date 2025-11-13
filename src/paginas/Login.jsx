548992c763e24f9e78f328b4ccd787a45d26e0c3
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2025-11-12 19:31:15 -0600

    Styled Login form

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,5 +1,6 @@
 import './Login.css'
 import loginImage from '../assets/movie-theater.jpg'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     const backgroundStyle = {
@@ -9,11 +10,32 @@ const Login = () => {
     padding: '10px'
   };
     return(
-        <div className="loginContainer">
-            <div className="contenedorImagen" style={{
-    background: `linear-gradient(rgba(0, 0, 50, 0.5), rgba(0, 0, 50, 0.5)), url('${loginImage}') center/cover no-repeat`,
-  }}>
-            </div>
+        <div className="loginContainer" style={{
+          background: `linear-gradient(rgba(0, 0, 50, 0.5), rgba(0, 0, 50, 0.5)), url('${loginImage}') center/cover no-repeat`,
+        }}> 
+          <div className="contenedorFormulario">
+            <h1>Iniciar sesión</h1>
+            <form action="">
+              <div className="inputGroup">
+                <input type="email" id="email" name="email" minLength="4" maxLength="70" size="10" autoComplete="off" placeholder=" " required/>
+                <label htmlFor="">Correo electrónico</label>
+              </div>
+              <div className="passwordGroup">
+                <div className="inputGroup">
+                  <input type="password" id="password" name="password" minLength="4" maxLength="50" size="10" autoComplete="off" placeholder=" " required/>
+                  <label htmlFor="">Contraseña</label>
+                </div>
+                <div className="visibilityToggleContainer">
+                  <span className="material-symbols-outlined">
+                    visibility
+                  </span>
+                </div>
+              </div>
+              <input type="submit" name='submit' value= "Iniciar sesión"/>
+              <Link className='recuperarPassword'>Olvidé mi constraseña.</Link>
+              <span>¿Aún no tienes una cuenta? <Link className='crearCuenta'>Regístrate aquí.</Link></span>
+            </form>
+          </div>
         </div>
     );
 }
