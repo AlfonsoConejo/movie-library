f99ece0c3d5e6e23b5b0e6ac556defd77d9b14c7
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2025-11-16 19:50:48 -0600

    Started register form

--- src/paginas/Registro.jsx
+++ src/paginas/Registro.jsx
@@ -1,8 +1,29 @@
 import './Registro.css'
+import HeaderSimple from '../components/HeaderSimple/HeaderSimple';
+import registerImage from '../assets/clapperboard.jpg'
+
+const handleSubmit = () => {
+
+};
 
 const Registro = () => {
     return(
-        <></>
+        <div className="registerPage">
+            <HeaderSimple/>
+            <div
+                className="registerContainer"
+                style={{
+                    background: `linear-gradient(rgba(43, 50, 0, 0.5), rgba(43, 50, 0, 0.5)), url('${registerImage}') center/cover no-repeat`,
+                }}
+            >
+                <div className="contenedorFormulario">
+                    <h1>Crea una cuenta</h1>
+                    <form onSubmit={handleSubmit}>
+
+                    </form>
+                </div>
+            </div> 
+        </div>
     );
 }
 
