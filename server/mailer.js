567056eca7c448bdb984613b2361261c8b93f1dd
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2025-11-21 19:24:08 -0600

    Create mailer.js

---
+++ server/mailer.js
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.office365.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.OUTLOOK_USER,
+    pass: process.env.OUTLOOK_PASS,
+  },
+});
+
+export async function enviarCorreo(opciones) {
+  try {
+    const info = await transporter.sendMail(opciones);
+    console.log("Correo enviado:", info.messageId);
+    return info;
+  } catch (error) {
+    console.error("Error enviando correo:", error);
+    throw error;
+  }
+}
