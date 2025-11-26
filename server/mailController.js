0d510685d7584a75eccc6e1068f4acb381e856c0
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2025-11-25 18:36:05 -0600

    Fixed mail transporter
    -It works but emails aren't received due to the lack of company email domain

--- server/mailController.js
+++ server/mailController.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export async function enviarCorreoDeRegistro(email) {
+  console.log(`Enviando al correo ${email}`);
   await enviarCorreo({
-    from: `"Mi App" <${process.env.OUTLOOK_USER}>`,
+    from: `"Brible" <${process.env.BREVO_USER}>`,
     to: email,
     subject: "Bienvenido a Mi App",
     html: `<h1>¡Hola!</h1><p>Gracias por registrarte.</p>`,
--- server/mailer.js
+++ server/mailer.js
@@ -2,20 +2,25 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-try{
-  const transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.OUTLOOK_USER,
-      pass: process.env.OUTLOOK_PASS,
-    },
-  });
-} catch (err){
-  console.log("Error en las credenciales" + err);
-}
+// Define el transporter globalmente
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
+  },
+});
 
+// Opcional: verificar la conexión SMTP
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("Error en SMTP:", error);
+  } else {
+    console.log("Conexión SMTP válida!");
+  }
+});
 
 export async function enviarCorreo(mailOptions) {
   try {
