b339592a8764c108a609870a9194b5892c6a17ba
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2025-11-26 16:53:01 -0600

    Implemented Gmail OAuth2

--- server/mailer.js
+++ server/mailer.js
@@ -1,29 +1,36 @@
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
 import dotenv from "dotenv";
 dotenv.config();
 
-// Define el transporter globalmente
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,
-  },
-});
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground" // mismo redirect del Playground
+);
 
-// Opcional: verificar la conexión SMTP
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("Error en SMTP:", error);
-  } else {
-    console.log("Conexión SMTP válida!");
-  }
-});
+
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 export async function enviarCorreo(mailOptions) {
   try {
+    // 1. Obtener access token fresco
+    const accessToken = await oAuth2Client.getAccessToken();
+
+    // 2. Crear transporter con OAuth2
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.GMAIL_USER,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken.token, // importante: .token
+      },
+    });
+
+    // 3. Enviar correo
     const info = await transporter.sendMail(mailOptions);
     console.log("Correo enviado:", info.messageId);
     return info;
