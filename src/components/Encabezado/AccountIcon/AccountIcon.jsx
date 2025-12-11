2455118a8984e5c584d5b6df73b83664b30ca40b
Alfonso Conejo <49042515+AlfonsoConejo@users.noreply.github.com>
2025-12-10 20:56:19 -0600

    Updated account icon behavior on header
    -Changed account icon
    -Erased tooltip feature when hovering the account icon

--- src/components/Encabezado/AccountIcon/AccountIcon.jsx
+++ src/components/Encabezado/AccountIcon/AccountIcon.jsx
@@ -2,15 +2,16 @@ import './AccountIcon.css'
 import TooltipAccount from '../TooltipAccount/TooltipAccount';
 import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import userIcon from '../../../assets/user-icon.png'
+import userIcon from '../../../assets/account_icon.png'
 
 
 export default function AccountIcon(){
+    /*
     const [open, setOpen] = useState(false);
-    const closeTimeout = useRef(null);
+    const closeTimeout = useRef(null);*/
     const navigate = useNavigate();
 
-    const openTooltip = () => {
+    /*const openTooltip = () => {
         clearTimeout(closeTimeout.current);
         setOpen(true);
     }
@@ -19,19 +20,19 @@ export default function AccountIcon(){
         closeTimeout.current = setTimeout( () => {
             setOpen(false);
         }, 250);
-    }
+    }*/
 
     return (
         <div 
             className="contenedorFlexAccount"
-            onMouseEnter={openTooltip}
-            onMouseLeave={closeTooltip}
+            {...{} /* onMouseEnter={openTooltip} */}
+            {...{} /* onMouseLeave={closeTooltip} */}
         >
             <button className="accountButton" >
                 <img src={userIcon} onClick={() => {navigate('/login')}}/>
             </button>
 
-            <TooltipAccount visible={open} />
+            {/*<TooltipAccount visible={open} />*/}
         </div>
     );
 }
